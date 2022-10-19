18f8d9648d5cb7f66688244c8587d55ad568d360
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-10-19 14:33:22 +0100

    added spring initialiser template

---
+++ world-pin-backend/src/main/java/com/example/worldpinbackend/WorldPinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.worldpinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldPinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldPinBackendApplication.class, args);
+	}
+
+}
---
+++ world-pin-backend/src/test/java/com/example/worldpinbackend/WorldPinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worldpinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldPinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
