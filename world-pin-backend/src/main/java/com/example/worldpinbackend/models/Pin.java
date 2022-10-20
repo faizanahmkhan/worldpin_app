90db1b950ef5fc7078bc89b57b1aca9c1668fa54
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-10-20 11:06:37 +0100

    created models

--- world-pin-backend/src/main/java/com/example/worldpinbackend/models/Pin.java
+++ world-pin-backend/src/main/java/com/example/worldpinbackend/models/Pin.java
@@ -1,4 +1,90 @@
 package com.example.worldpinbackend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "pins")
 public class Pin {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column (name = "description")
+    private String description;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @Column (name = "date")
+    private Date date;
+
+    @Column (name = "location")
+    private String location;
+
+    @Column (name = "image_id")
+    private long image_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties({"pins"})
+    private User user;
+
+    public Pin(String description, Date date, String location, long image_id, User user) {
+        this.description = description;
+        this.date = date;
+        this.location = location;
+        this.image_id = image_id;
+        this.user = user;
+    }
+
+    public Pin() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public long getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(long image_id) {
+        this.image_id = image_id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- world-pin-backend/src/main/java/com/example/worldpinbackend/models/User.java
+++ world-pin-backend/src/main/java/com/example/worldpinbackend/models/User.java
@@ -1,4 +1,54 @@
 package com.example.worldpinbackend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "userName")
+    private String userName;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnoreProperties({"user"})
+    private List<Pin> pins;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.pins = new ArrayList<>();
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<Pin> getPins() {
+        return pins;
+    }
+
+    public void setPins(List<Pin> pins) {
+        this.pins = pins;
+    }
 }
