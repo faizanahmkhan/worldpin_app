729c9edeefdf3d42d5f9c699e056ce79ea4b0e9c
Kai Chan <owenkaic@gmail.com>
2022-10-27 23:26:51 +0100

    pre-uploaded pin data

--- world-pin-backend/src/main/java/com/example/worldpinbackend/components/DataLoader.java
+++ world-pin-backend/src/main/java/com/example/worldpinbackend/components/DataLoader.java
@@ -31,17 +31,19 @@ public class DataLoader implements ApplicationRunner {
         User user2 = new User("Hasmeet");
         User user3 = new User("Faizan");
         User user4 = new User("Kai");
+        User user5 = new User("Ravi");
 
-//        Pin pin1 = new Pin("Swag description1", new Date(1 - 10 - 2022), "London", "77", user1);
-//        Pin pin2 = new Pin("Swag description2", new Date(2 - 10 - 2021), "Barcelona", "66", user1);
-//        Pin pin3 = new Pin("Swag description3", new Date(3 - 10 - 2020), "Paris", "44", user2);
-//        Pin pin4 = new Pin("Swag description4", new Date(4 - 10 - 2019), "Krakow", "55", user2);
-//        Pin pin5 = new Pin("Swag description5", new Date(5 - 10 - 2018), "Berlin", "34", user3);
-//        Pin pin6 = new Pin("Swag description6", new Date(6 - 10 - 2017), "Amsterdam", "54", user3);
-//        Pin pin7 = new Pin("Swag description7", new Date(7 - 10 - 2017), "Budapest", "11", user4);
-//        Pin pin8 = new Pin("Swag description8", new Date(8 - 10 - 2017), "Rome", "47", user4);
 
-//        pinRepository.saveAll(Arrays.asList(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8));
+        Pin pin1 = new Pin("Got lost on my way to Ibiza", new Date(1 - 10 - 2022), "51.517,-0.077", "https://firebasestorage.googleapis.com/v0/b/worldpin-f05bd.appspot.com/o/images%2FIMG_20221027_144631_3.jpg1fc3d4e4-868c-43ec-aafe-59ae361ca530?alt=media&token=814654ad-63fd-407b-bd09-df3bdef1515d", user1);
+        Pin pin2 = new Pin("Dolphin Love", new Date(1 - 10 - 2022), "39.3999,8.2245", "https://firebasestorage.googleapis.com/v0/b/worldpin-f05bd.appspot.com/o/images%2FWhatsApp%20Image%202022-10-27%20at%2015.50.21.jpege0c49db8-a00f-4840-8bbd-7424c2e41fc5?alt=media&token=9b96be8f-7a44-48a6-ad5e-8d8fca7b46e5", user2);
+        Pin pin3 = new Pin("Faizan", new Date(1 - 10 - 2022), "50.938250,-10.201940", "https://firebasestorage.googleapis.com/v0/b/worldpin-f05bd.appspot.com/o/images%2FWhatsApp%20Image%202022-10-27%20at%2015.50.21.jpege0c49db8-a00f-4840-8bbd-7424c2e41fc5?alt=media&token=9b96be8f-7a44-48a6-ad5e-8d8fca7b46e5", user3);
+        Pin pin4 = new Pin("Beer pong got competitive", new Date(1 - 10 - 2022), "55.938250,-3.201940", "https://firebasestorage.googleapis.com/v0/b/worldpin-f05bd.appspot.com/o/images%2FBeerPong.jpeg0884dec9-5afb-4939-83c5-62f3ebd07886?alt=media&token=9ae74314-48a4-4a30-a108-48d999700e69", user4);
+        Pin pin5 = new Pin("Slow animal drinking in Matobo national park", new Date(1 - 10 - 2022), "20.5572, 28.5125", "https://firebasestorage.googleapis.com/v0/b/worldpin-f05bd.appspot.com/o/images%2FIMG_1814.JPGc9ea46f1-834f-49a9-8053-1517e55a081b?alt=media&token=77c6d158-abd9-430a-8326-5fd1b45057ec", user5);
+
+
+
+
+        pinRepository.saveAll(Arrays.asList(pin1, pin2, pin3, pin4, pin5));
         userRepository.saveAll(Arrays.asList(user1, user2, user3, user4));
 
     }
